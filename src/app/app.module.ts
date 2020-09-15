393e63fa8fa1e999590306175cde84f0a4dfdb71
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-09-15 09:30:01 +0200

    Header extracted from landing page into its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProductsComponent } from './products/products.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ProductsComponent
+    ProductsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
