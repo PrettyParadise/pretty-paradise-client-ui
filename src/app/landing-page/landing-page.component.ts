439f85909135309cf4cd3227206b9767e43d3158
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-09-15 13:34:34 +0200

    Get all products service added, all products can be viewed in products component and the latest 3 products are added to the carousel

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BackendCallService} from "../core/services/backend-call.service";
+import {ProductModel} from "../core/models/product.model";
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  latestAddedProducts: ProductModel[] = [];
+  latestAddedProductImages: string[] = [];
 
-  constructor() { }
+  constructor(private backendCallService: BackendCallService) {
+  }
 
   ngOnInit(): void {
+    this.backendCallService.getAllProducts().subscribe(
+      (products) => {
+        for (let i = products.length - 1; i > products.length - 4; i--){
+          this.latestAddedProducts.push(products[i]);
+          this.latestAddedProductImages.push(`data:image/png;base64,${products[i].encodedImage || ''}`);
+        }
+      }
+    );
   }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BackendCallService} from "../core/services/backend-call.service";
+import {ProductModel} from "../core/models/product.model";
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: ProductModel[] = null;
+  productImages: string[] = [];
 
-  constructor() { }
+  constructor(private backendCallService: BackendCallService) {
+  }
 
   ngOnInit(): void {
+    this.backendCallService.getAllProducts().subscribe(
+      (products) => {
+        this.products = products;
+        this.productImages = [];
+        for (let product of this.products) {
+          this.productImages.push(`data:image/png;base64,${product.encodedImage || ''}`);
+        }
+      }
+    );
   }
 
 }
