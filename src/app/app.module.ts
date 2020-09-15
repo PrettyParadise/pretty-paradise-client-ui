c9160c598653823627c0bf8ccdfb6c332a789ac6
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-09-15 09:12:48 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
